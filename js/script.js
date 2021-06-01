824954719d4d9101e701580e3631986e2b26631d
DamianoD88 <dispe88@live.it>
2021-06-01 15:25:54 +0200

    add esercizio

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+// L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// il primo prompt dove inserire la prima parola
+
+var firstword = prompt('prima parola');
+
+// stampo firstword
+console.log(firstword);
+
+
+// il secondo prompt dove inserire la seconda parola
+
+var secondword  = prompt('seconda parola');
+
+// stampo secondword
+console.log(secondword);
+
+if (firstword.length() > secondword.length()) {
+    document.getElementById 
+}
+
+
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri   inseriti. Esegui questo programma in due versioni, con il for e con il while.
+
+
+
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+
+
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
