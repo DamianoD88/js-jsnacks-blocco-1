236d85067077e42fe934aedb247ab001cd9e6256
DamianoD88 <dispe88@live.it>
2021-06-02 23:17:21 +0200

    add second ex

--- js/script.js
+++ js/script.js
@@ -11,28 +11,36 @@ console.log(firstWord);
 
 // il secondo prompt dove inserire la seconda parola
 // valore firstword uguale richiesta seconda parola
-var secondWord  = prompt('seconda parola');
+// var secondWord  = prompt('seconda parola');
 
 // stampo secondword
-console.log(secondWord);
+// console.log(secondWord);
 
 
 
-if (firstWord.length > secondWord.length) {
-    document.getElementById('word').innerHTML = 'più corta'+ secondWord;
+// if (firstWord.length > secondWord.length) {
+//     document.getElementById('word').innerHTML = 'più corta'+ secondWord;
 
-}  else if (firstWord.length < secondWord.length) {
-    document.getElementById('word').innerHTML = 'più corta'+ firstWord;
-}
-    else  {
-        document.getElementById('word').innerHTML = 'parole uguali';
-} 
+// }  else if (firstWord.length < secondWord.length) {
+//     document.getElementById('word').innerHTML = 'più corta'+ firstWord;
+// }
+//     else  {
+//         document.getElementById('word').innerHTML = 'parole uguali';
+// } 
 
 
 
 // Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri   inseriti. Esegui questo programma in due versioni, con il for e con il while.
 
+var somma = 0;
 
+for (var i = 0; i < 5; i++) {
+    numero = parseInt(prompt('inserire numero'));
+    console.log(numero);
+    somma = somma + numero;
+}
+
+console.log(somma)
 
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
 
